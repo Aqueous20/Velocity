5c9201f2cb3002f4353e08fee2f0046c58443e46
Corey Anderson <kingcoreyanderson@gmail.com>
2025-10-03 15:47:21 -0500

    Update CarSlider.jsx
    updated car slider component

--- velocity/src/Components/CarSlider.jsx
+++ velocity/src/Components/CarSlider.jsx
@@ -1,13 +1,54 @@
-import React from 'react'
+import React from "react";
 
-import engine from '../assets/icons/carSlider/engine.svg'
-import gas from '../assets/icons/carSlider/gas.svg'
-import gearshift from '../assets/icons/carSlider/gearshift.svg'
-import seat from '../assets/icons/carSlider/seat.svg'
-import wheel from '../assets/icons/carSlider/wheel.svg'
+import engine from "../assets/icons/carSlider/engine.svg";
+import gas from "../assets/icons/carSlider/gas.svg";
+import gearshift from "../assets/icons/carSlider/gearshift.svg";
+import seat from "../assets/icons/carSlider/seat.svg";
+import wheel from "../assets/icons/carSlider/wheel.svg";
+
+import car1 from "../assets/images/carSlider/car01.svg";
+import car2 from "../assets/images/carSlider/car02.svg";
+import car3 from "../assets/images/carSlider/car03.svg";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+
+import motion from "motion/react";
+
+const cars = [
+  {
+    type: "Hatchback",
+    name: "Ford Focus",
+    price: 29,
+    stars: 3.5,
+    image: { car1 },
+    info: [
+      {
+        icon: { gearshift },
+        text: "Manual",
+      },
+      {
+        icon: { seat },
+        text: "5 Seats",
+      },
+      {
+        icon: { gas },
+        text: "Gas",
+      },
+      {
+        icon: { engine },
+        text: "1600 HP",
+      },
+      {
+        icon: { wheel },
+        text: "Front",
+      },
+    ],
+  },
+];
 
 export const CarSlider = () => {
-  return (
-    <div>CarSlider</div>
-  )
-}
+  return <div>CarSlider</div>;
+};
